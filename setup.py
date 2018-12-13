7be669a474023ff515fe6ef8135152b3874896df
Ewan Oglethorpe <oglethorpe.ewan@gmail.com>
2018-12-13 19:20:34 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='drafter',
+      version='1.0',
+      description='layer ontop of Pango and Cairo for report generation',
+      author='bibekdahal',
+     )
