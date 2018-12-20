8cfde0517b532995a4661b5a0416709bea407779
Ewan Oglethorpe <oglethorpe.ewan@gmail.com>
2018-12-20 21:44:57 +0000

    m00r test

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
 from drafter.draft import PdfDraft
-from drafter.nodes import NP_Text
+from drafter.nodes import Text
 from drafter.layouts import Column
 from drafter.utils import Rect
 
-PdfDraft('./test.pdf').draw(
-    Column(width = 1000,
-           height = 1000).add(
-        NP_Text(
-            text = 'जिमाली',
-            font='Roboto Light 8',
-            padding=Rect(10)
-        )
-    )
+import cairo
+import gi
+gi.require_version('PangoCairo', '1.0')  # noqa
+
+from gi.repository import (
+    Pango,
+    PangoCairo,
 )
+
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 1000, 1000)
+context = cairo.Context(surface)
+layout = PangoCairo.create_layout(context)
+
+layout.set_wrap(Pango.WrapMode.WORD)
+layout.set_font_description(Pango.FontDescription("Arial 10"))
+layout.set_text("The Quick Brown Fox Jumps Over The Piqued Gymnast", -1)
+
+PangoCairo.update_layout(context, layout)
+PangoCairo.show_layout(context, layout)
+
+context.show_page()
+
+1
